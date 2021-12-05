210d1e8de8fe69760a9b3f71437eadc2c7e7c995
Dave <dave.gagnat@gmail.com>
2021-12-05 01:26:25 -0600

    Solved ford_films, ford_supporting_films, films_and_stars_from_sixty_two, travoltas_busiest_years

--- lib/07_joins_exercises.rb
+++ lib/07_joins_exercises.rb
@@ -40,6 +40,16 @@ end
 def ford_films
   # List the films in which 'Harrison Ford' has appeared.
   execute(<<-SQL)
+    SELECT
+      title
+    FROM
+      movies
+    INNER JOIN
+      castings ON castings.movie_id = movies.id
+    INNER JOIN
+      actors ON castings.actor_id = actors.id
+    WHERE
+      actors.name = 'Harrison Ford';
   SQL
 end
 
@@ -48,12 +58,33 @@ def ford_supporting_films
   # role. [Note: the ord field of casting gives the position of the actor. If
   # ord=1 then this actor is in the starring role]
   execute(<<-SQL)
+    SELECT
+      title
+    FROM
+      movies
+    INNER JOIN
+      castings ON castings.movie_id = movies.id
+    INNER JOIN
+      actors ON castings.actor_id = actors.id
+    WHERE
+      actors.name = 'Harrison Ford' AND castings.ord != 1;
   SQL
 end
 
 def films_and_stars_from_sixty_two
   # List the title and leading star of every 1962 film.
   execute(<<-SQL)
+    SELECT
+      movies.title,
+      actors.name
+    FROM
+      movies
+    INNER JOIN
+      castings ON castings.movie_id = movies.id
+    INNER JOIN
+      actors ON castings.actor_id = actors.id
+    WHERE
+      castings.ord = 1 AND movies.yr = 1962;
   SQL
 end
 
@@ -61,6 +92,21 @@ def travoltas_busiest_years
   # Which were the busiest years for 'John Travolta'? Show the year and the
   # number of movies he made for any year in which he made at least 2 movies.
   execute(<<-SQL)
+    SELECT
+      movies.yr,
+      COUNT(movies.title)
+    FROM
+      movies
+    INNER JOIN
+      castings ON castings.movie_id = movies.id
+    INNER JOIN
+      actors ON castings.actor_id = actors.id
+    WHERE
+      actors.name = 'John Travolta'
+    GROUP BY
+      movies.yr
+    HAVING
+      COUNT(movies.title) >= 2;
   SQL
 end
 
